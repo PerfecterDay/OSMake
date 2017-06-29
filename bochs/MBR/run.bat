4b8a632805d34d6e3146281f804142cc588fd795
zzwang <zzwang@chinaums.com>
2017-06-29 09:50:06 +0800

    目录结构调整

---
+++ bochs/MBR/run.bat
@@ -0,0 +1,2 @@
+dd if=../../scr/mbr.bin of=./hd10meg.img bs=512 count=1 conv=notrunc
+"D:\Program Files\Bochs-2.6.9\bochsdbg" -f bochsrc.disk
--- bochs/run.bat
+++
@@ -1,2 +0,0 @@
-dd if=../scr/mbr.bin of=./hd10meg.img bs=512 count=1 conv=notrunc
-"D:\Program Files\Bochs-2.6.9\bochsdbg" -f bochsrc.disk
